67699d01b4f3267b8d23ff9cee1c64cb88563728
Sothyrith Hy <sothyrithhy@gmail.com>
2021-01-03 16:32:24 +0700

    Adds id to RoomTypeResource

--- Desktop/Laravel/project/hotel_system/app/Http/Resources/RoomTypeResource.php
+++ Desktop/Laravel/project/hotel_system/app/Http/Resources/RoomTypeResource.php
@@ -15,6 +15,7 @@ class RoomTypeResource extends JsonResource
     public function toArray($request)
     {
         return [
+            "id" => $this->id,
           "name" => $this->name,
           "price_per_room" => $this->price,
           "quantity" => count($this->rooms),
